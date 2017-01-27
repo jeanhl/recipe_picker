d3c47a096e45ea7ad9444e905d8a4c8c7e751666
Jean Liu <hjliu415@gmail.com>
2017-01-27 11:23:26 -0800

    cleaned up code. Added in a sender filter for security purposes.

--- rp_main.py
+++ rp_main.py
@@ -6,46 +6,39 @@ import quopri
 import numpy as np
 import pdb
 
-def read(username, password):
-    # Login to INBOX
-    imap = imaplib.IMAP4_SSL("imap.gmail.com", 993)
-    imap.login(username, password)  
-    imap.select('INBOX')
 
-    # Use search(), not status()
-    status, response = imap.search(None, 'UNSEEN')
-    unread_msg_nums = response[0].split()
+def read(username, password, sender):
+    # username = username of gmail that accepts the recipes and commands
+    # password = password of said gmail account
+    # sender = for security purposes, only get text body from certain sender
+    # sender format = "First Last <sender@gmail.com>"
+    # connecting to gMail's imap, logging in and going into INBOX
+    mail = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+    mail.login(username, password)
+    mail.select('INBOX')  # going into the inbox
 
-    # Print the count of all unread messages
-    print len(unread_msg_nums)
+    status, response = mail.search(None, 'UNSEEN')  # only looking at unread emails
+    new_email_ids = response[0].split()  # response is a list of string ['1 2 3 4']
+    print response
 
-    # Print all unread messages from a certain sender of interest
-    # status, response = imap.search(None, 'UNSEEN')  # OK ['email_id']
-
-    # unread_msg_nums = response[0].split()
-    da = []
-    for e_id in unread_msg_nums:
-        _, response = imap.fetch(e_id, '(UID BODY[TEXT])')  # OK [ response, in this case, body]
-        # print "_, response", _, response[0][0], response[0][1]
-        #text2 = BeautifulSoup(response[0][1], 'lxml')
-        print quopri.decodestring(get_plain_text(response[0][1]))
-        # print "--------------------------------", quopri.decodestring(response[0][1]), "--------------------------------"
-        # print text2.get_text()
-        da.append(response[0][1])
-        print imap.fetch(e_id, '(BODY[HEADER.FIELDS (FROM)])')[1][0][1]  # 'From: Google <no-reply@accounts.google.com>''
-        subject = imap.fetch(e_id, '(BODY[HEADER.FIELDS (SUBJECT)])')[1][0][1]
-        print subject
-    # print da
+    for e_id in new_email_ids:
+        sender = mail.fetch(e_id, '(BODY[HEADER.FIELDS (FROM)])')[1][0][1]  # 'From: Google <no-reply@accounts.google.com>'
+        print sender, type(sender)
+        if sender[6:].strip() == sender:
+            _, response = mail.fetch(e_id, '(UID BODY[TEXT])')  # returns OK [body]
+            print quopri.decodestring(get_plain_text(response[0][1]))
+            subject = mail.fetch(e_id, '(BODY[HEADER.FIELDS (SUBJECT)])')[1][0][1]
+            print sender, subject
 
     # Mark them as seen
     # for e_id in unread_msg_nums:
-    #     imap.store(e_id, '+FLAGS', '\Seen')
+    #     mail.store(e_id, '+FLAGS', '\Seen')
 
 
 def get_plain_text(text):
     first_line_break = text.index('\r')
     gibberish = text[:first_line_break]
     second_gibberish_index = text.index(gibberish, first_line_break)
-    printable_index = text.index('quoted-printable')
-    plain_text = text[(printable_index + 16):second_gibberish_index].strip()
+    start_index = text.index('\r\n\r\n')
+    plain_text = text[(start_index):second_gibberish_index].strip()
     return plain_text
